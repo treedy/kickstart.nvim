e2e5acf542e99cef7ac49d93108d22c411a8060b
Todd Reedy <1851066+treedy@users.noreply.github.com>
2024-01-06 09:37:30 -0500

    Install Obsidian for Neovim plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -64,4 +64,23 @@ return {
       vim.cmd.colorscheme('catppuccin-frappe')
     end
   },
+  {
+    'epwalsh/obsidian.nvim',
+    version = '*',
+    lazy = true,
+    ft = 'markdown',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      workspaces = {
+        {
+          name = 'playground',
+          path = '/mnt/nasshare/obsidian/playground',
+        },
+        {
+          name = 'personal',
+          path = '/mnt/nasshare/obsidian/personal',
+        },
+      },
+    },
+  },
 }
