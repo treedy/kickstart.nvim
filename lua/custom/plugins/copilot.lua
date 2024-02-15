21dee5036cb5652757b539de231efc1a472b01ce
Todd Reedy <1851066+treedy@users.noreply.github.com>
2024-02-15 08:59:48 -0500

    Change tab completion for GitHub Copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,5 +1,12 @@
 return {
   {
     'github/copilot.vim',
+    init = function ()
+      vim.keymap.set('i', '<M-;>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+      vim.g.copilot_no_tab_map = true
+    end
   },
 }
