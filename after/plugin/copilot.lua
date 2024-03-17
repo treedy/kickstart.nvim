0c91d5179b8743fc8179ecee154fc182f7dbd1e4
Todd Reedy <1851066+treedy@users.noreply.github.com>
2024-03-17 14:01:47 -0400

    Adjust copilot key mappings

--- after/plugin/copilot.lua
+++
@@ -1,4 +0,0 @@
--- Copilot mappings
-vim.keymap.set('i', '<C-Space>', '<Plug>(copilot-suggest)')
-vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
-vim.keymap.set('i', '<C-M-L>', '<Plug>(copilot-accept-line)')
--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -2,9 +2,11 @@ return {
   {
     'github/copilot.vim',
     init = function ()
+      vim.keymap.set('i', '<M-j>', '<Plug>(copilot-suggest)')
+      vim.keymap.set('i', '<M-k>', '<Plug>(copilot-accept-word)')
+      vim.keymap.set('i', '<M-l>', '<Plug>(copilot-accept-line)')
       vim.keymap.set('i', '<M-;>', 'copilot#Accept("\\<CR>")', {
         expr = true,
-        replace_keycodes = false
       })
       vim.g.copilot_no_tab_map = true
     end
