d6cef59648de4be4faa79e36ed791b4e049a5e09
Todd Reedy <1851066+treedy@users.noreply.github.com>
2024-04-05 09:39:11 -0400

    feat: Replace cmp-cmdline with noice.nvim
    This commit replaces the 'hrsh7th/cmp-cmdline' plugin with 'folke/noice.nvim'.
    'noice.nvim' provides similar functionality and is expected to
    improve the user experience. The 'noice.nvim' plugin is configured with
    default settings and some optional dependencies are also added.

--- init.lua
+++ init.lua
@@ -107,7 +107,9 @@ require('lazy').setup({
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
 
-      'hrsh7th/cmp-cmdline',
+      -- 'hrsh7th/cmp-cmdline',
+      -- From https://github.com/hrsh7th/cmp-cmdline/issues/108#issuecomment-2003410847
+
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
 
@@ -628,24 +630,6 @@ cmp.setup {
     { name = 'path' },
   },
 }
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline({
-    -- <C-n/p> will handle history. Use <Tab> for completion.
-    ['<C-n>'] = {
-      c = false,
-    },
-    ['<C-p>'] = {
-      c = false,
-    },
-  }),
-  sources = {
-    { name = 'cmdline' },
-    { name = 'path' },
-  },
-  formatting = {
-    fields = { 'kind', 'abbr' },
-  },
-})
 
 require('custom')
 -- The line beneath this is called `modeline`. See `:help modeline`
